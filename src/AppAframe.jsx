fa912cdf4a3c26d4f80e9eb21ee13d6131532daa
daniel echeveste <d.echeveste-gonzalez@ovan.de>
2024-10-21 10:30:46 +0200

    #

--- src/AppAframe.jsx
+++ src/AppAframe.jsx
@@ -3,7 +3,7 @@ export default function AppAframe() {
     return (
         <>
         
-            <a-scene cursor="rayOrigin:xrselect;" raycaster="objects: [mixin=mouseover]">
+            <a-scene cursor="rayOrigin:xrselect;" raycaster="objects: [mixin=mouseover]" arjs="sourceType: webcam;">
                 <a-assets>
                     <a-mixin id='mouseover'>
                     </a-mixin>
@@ -12,7 +12,7 @@ export default function AppAframe() {
                 <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
                 <a-cylinder position="3 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
                 <a-plane position="0 0 -4" rotation="-90 0 0" width="10" height="10" color="#7BC8A4"></a-plane>
-                <a-sky color="#ECECEC"></a-sky>
+                {/* <a-sky color="#ECECEC"></a-sky> */}
             </a-scene>
         </>
     )
