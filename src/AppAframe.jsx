0c9f904544606d49da9820c14b8a2febda110f98
daniel echeveste <d.echeveste-gonzalez@ovan.de>
2024-10-22 10:16:17 +0200

    #

--- src/AppAframe.jsx
+++ src/AppAframe.jsx
@@ -8,13 +8,11 @@ export default function AppAframe() {
         <>
             <a-scene real-world-meshing cursor="rayOrigin:xrselect;" raycaster="objects: [mixin=mouseover]" >
                 <a-assets>
-                    <a-asset-item id="helmet" src="/FlightHelmet/glTF/FlightHelmet.gltf"></a-asset-item>
                     <a-asset-item id="burger" src="/hamburger.glb"></a-asset-item>
                     <a-asset-item id="bot" src="/puter_bot.glb"></a-asset-item>
                 </a-assets>
 
                 {/* <a-box position="-3 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box> */}
-                <a-entity position="1 1.25 -2" gltf-model="#helmet" src="#helmet"></a-entity>
                 <a-entity position="0 1.25 -2" gltf-model="#burger" src="#burger" scale="0.1 0.1 0.1"></a-entity>
                 <a-entity position="0 5 -4" gltf-model="#bot" src="#bot" ></a-entity>
 
