23d459eccb5425e33aeec9fe64379bc661b17993
daniel echeveste <d.echeveste-gonzalez@ovan.de>
2024-10-22 10:09:49 +0200

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,53 @@
 import { Canvas } from '@react-three/fiber'
-import { XR, createXRStore } from '@react-three/xr'
 import { useState } from 'react'
 import { SphereGeometry, } from 'three'
-import { OrbitControls, Html,Float, Stage, } from '@react-three/drei'
+import { OrbitControls, Html, Float, Stage, } from '@react-three/drei'
+import Experience from './Experience.jsx'
 import AppAframe from './AppAframe.jsx'
 import AppXR from './AppXR.jsx'
+import { createXRStore, XR } from '@react-three/xr'
+
+
 
 import ReactDOM from 'react-dom/client'
 
+const store = createXRStore()
 
 export default function App() {
-  const [XR, setIsXR] = useState(false)
-  const [Aframe, setIsAframe] = useState(false);
-  const enterAframe = ()=>{
-    setIsAframe(!Aframe);
-  }
-  const enterXR = ()=>{
-    setIsXR(!XR);
-  }
-  
-  
+  // const [XR, setIsXR] = useState(false)
+  // const [Aframe, setIsAframe] = useState(false);
+  // const enterAframe = () => {
+  //   setIsAframe(!Aframe);
+  // }
+  // const enterXR = () => {
+  //   setIsXR(!XR);
+  // }
+
+
   return (
     <>
-    <AppXR></AppXR>
-            {/* {!Aframe && <button onClick={enterXR} className='enter-xr-button'>Enter VR</button>} */}
-            {/* {XR && <AppXR></AppXR>} 
+      {/* <AppXR></AppXR> */}
+      {/* {!Aframe && <button onClick={enterXR} className='enter-xr-button'>Enter VR</button>} */}
+      {/* {XR && <AppXR></AppXR>} 
             {!XR && <button onClick={enterAframe} className='enter-vr-button'>Enter AR</button> }
             {Aframe && <AppAframe></AppAframe>} */}
+      {/* <AppAframe></AppAframe> */}
+      <button onClick={() => store.enterVR()}>Enter VR</button>
+      <button onClick={() => store.enterAR()}>Enter AR</button>
+      <Canvas
+
+        shadows={true}
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [- 4, 3, 6]
+        }}
+      >
+        <XR store={store}>
+          <Experience />
+        </XR>
+      </Canvas>
     </>
   )
 }
--- src/AppAframe.jsx
+++ src/AppAframe.jsx
@@ -1,18 +1,27 @@
+import { Canvas } from '@react-three/fiber'
+import { useState } from 'react'
+import { Float } from '@react-three/drei'
+import { SphereGeometry, } from 'three'
 export default function AppAframe() {
+   
     return (
         <>
-        
-            <a-scene embedded xr-mode="ar" cursor="rayOrigin:xrselect;" raycaster="objects: [mixin=mouseover]" arjs="sourceType: webcam;">
+            <a-scene real-world-meshing cursor="rayOrigin:xrselect;" raycaster="objects: [mixin=mouseover]" >
                 <a-assets>
-                    <a-mixin id='mouseover'>
-                    </a-mixin>
+                    <a-asset-item id="helmet" src="/FlightHelmet/glTF/FlightHelmet.gltf"></a-asset-item>
+                    <a-asset-item id="burger" src="/hamburger.glb"></a-asset-item>
+                    <a-asset-item id="bot" src="/puter_bot.glb"></a-asset-item>
                 </a-assets>
-                {/* <a-marker-camera preset='hiro'></a-marker-camera> */}
-                <a-box position="-3 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-                <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+
+                {/* <a-box position="-3 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box> */}
+                <a-entity position="1 1.25 -2" gltf-model="#helmet" src="#helmet"></a-entity>
+                <a-entity position="0 1.25 -2" gltf-model="#burger" src="#burger" scale="0.1 0.1 0.1"></a-entity>
+                <a-entity position="0 5 -4" gltf-model="#bot" src="#bot" ></a-entity>
+
+                {/* <a-sphere position="0 1.25 -5" radius="1.25" color="#ff00ff"></a-sphere> */}
                 <a-cylinder position="3 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
                 <a-plane position="0 0 -4" rotation="-90 0 0" width="10" height="10" color="#7BC8A4"></a-plane>
-                {/* <a-sky color="#ECECEC"></a-sky> */}
+                <a-sky color="#ECECCC"></a-sky>
             </a-scene>
         </>
     )
