8d1afc7d33092d704050a6367333c59862be24f1
danielecheveste <d.echeveste-gonzalez@ovan.de>
2024-11-29 16:42:15 +0100

    # modified matirials

--- src/shaders/modifiedMaterials/HeadModel.jsx
+++ src/shaders/modifiedMaterials/HeadModel.jsx
@@ -1,9 +1,56 @@
-import { useGLTF } from "@react-three/drei"
+import { useGLTF, useTexture } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber";
 
-export default function HeadModel(){
+export default function HeadModel(args){
     const model = useGLTF('models/LeePerrySmith/LeePerrySmith.glb')
-    console.log('pepe');
+    console.log(model);
+    model.materials["Material.002"].map = useTexture('models/LeePerrySmith/color.jpg')
+    // model.materials["Material.002"].color = useTexture('models/LeePerrySmith/color.jpg')
+    model.materials["Material.002"].normal = useTexture('models/LeePerrySmith/normal.jpg')
+    model.materials["Material.002"].normalMap = useTexture('models/LeePerrySmith/normal.jpg')
+
+
+   
+    const customUniforms = {
+        uTime : {value:0}
+    }
+
+    useFrame((state, delta) => {
+        customUniforms.uTime.value = state.clock.elapsedTime;
+    })
+
+    model.materials["Material.002"].onBeforeCompile = (shader)=>{
+        console.log(shader.vertexShader)
+        shader.uniforms.uTime = customUniforms.uTime
+        shader.vertexShader = shader.vertexShader.replace( 
+            '#include <common>',
+            `
+            #include <common>
+            
+            uniform float uTime;
+
+            mat2 get2dRotateMatrix(float _angle)
+            {
+                return mat2(cos(_angle), -sin(_angle), sin(_angle), cos(_angle));
+            }
+            
+            `)
+        shader.vertexShader = shader.vertexShader.replace(
+            '#include <begin_vertex>',
+            `
+            #include <begin_vertex>
+            
+            float angle = (position.y + uTime) * 0.8;
+            
+            mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+            transformed.xz = rotateMatrix * transformed.xz;
+            
+            `)
+    }
+    // model.materials[0].color = useTexture('models/LeePerrySmith/color.jpg')
     
-    return <primitive object={model.scene} scale={1}/>
+    
+    return <primitive object={model.scene} castShadow position-z={-4} scale={1}/>
 }
 useGLTF.preload('models/LeePerrySmith/LeePerrySmith.glb')
--- src/shaders/modifiedMaterials/shader.jsx
+++ src/shaders/modifiedMaterials/shader.jsx
@@ -8,10 +8,11 @@ import {
   shaderMaterial,
   Sky,
   Environment,
+  
 } from "@react-three/drei";
 
 import { useMemo, useState, useRef, Suspense } from "react";
-import { useFrame, extend } from "@react-three/fiber";
+import { useFrame, extend, } from "@react-three/fiber";
 import Placeholder from "../../Placeholder";
 import HeadModel from "./headModel";
 // import testVertexShader from './vertex.glsl'
@@ -21,16 +22,22 @@ import { useControls } from "leva";
 export default function ModifiedMaterial() {
   return (
     <>
-        
+    <ambientLight intensity={0.5}></ambientLight>
+    <directionalLightHelper></directionalLightHelper>
+    <directionalLight castShadow position={[4, 2, - 2.25]} intensity={10}></directionalLight>
       <Environment
         files={"textures/environmentMaps/red_wall_4k.hdr"}
         background
+        
       ></Environment>
       <Suspense fallback={<Placeholder position-y={0.5} scale={[2, 3, 2]} />}>
-        <HeadModel></HeadModel>
+        <HeadModel ></HeadModel>
       </Suspense>
-
-      <mesh></mesh>
+      
+      <mesh position={[-5,0,-4]} rotation={[0,Math.PI/2,0]} scale={[15,15,15]} receiveShadow>
+        <planeGeometry></planeGeometry>
+        <meshStandardMaterial color={"white"}></meshStandardMaterial>
+      </mesh>
     </>
   );
 }
