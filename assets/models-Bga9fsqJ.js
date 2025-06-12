09d2c7715d1cdeb797f989a3adf937dd1d785cfc
daniel-echeveste <danielechevestegonzalez@gmail.com>
2025-06-12 02:46:15 +0200

    Updates

--- assets/models-Bga9fsqJ.js
+++
@@ -1,2 +0,0 @@
-import{u as o}from"./index-D8glOjKT.js";o.preload("models/models.glb");
-//# sourceMappingURL=models-Bga9fsqJ.js.map
---
+++ assets/models-CWLbJEeC.js
@@ -0,0 +1,2 @@
+import{u as o}from"./index-nD5F6TBz.js";o.preload("models/models.glb");
+//# sourceMappingURL=models-CWLbJEeC.js.map
