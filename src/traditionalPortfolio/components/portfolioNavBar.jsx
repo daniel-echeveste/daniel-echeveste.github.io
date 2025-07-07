21749ee087189fdfd12f367cf5c88b308c076608
daniel-echeveste <danielechevestegonzalez@gmail.com>
2025-07-07 20:08:45 +0200

    #

--- src/traditionalPortfolio/components/portfolioNavBar.jsx
+++ src/traditionalPortfolio/components/portfolioNavBar.jsx
@@ -4,6 +4,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 export default function NavigationBar({ darkMode, onDarkModeToggle, isHorizontal, onHorizontalToggle, currentSection, onSectionChange }) {
 
+    const [menuOpen, setMenuOpen] = useState(false);
     const scrollToSection = (sectionId) => {
         const element = document.getElementById(sectionId);
         if (element) {
@@ -21,10 +22,13 @@ export default function NavigationBar({ darkMode, onDarkModeToggle, isHorizontal
         }
     };
 
+
+    const sections = ["about", "experience", "education", "projects", "skills", "certifications", "contact"];
+
     return (
         <div className={`text-white font-sans text-lg ${darkMode ? "bg-gray-900" : "bg-amber-950"}`}>
             <nav
-                className={`fixed top-0 w-full flex justify-between items-center transition-all duration-1000 
+                className={`fixed top-0 w-full min-h-[40px] flex justify-between items-center transition-all duration-1000 
       ${darkMode ? "bg-gray-900/80 drop-shadow-[0_2px_20px_rgba(255,255,255,0.2)]" : "bg-amber-950/80 drop-shadow-[0_2px_20px_#461901]"} 
        z-[100]`}
             >
@@ -39,8 +43,15 @@ export default function NavigationBar({ darkMode, onDarkModeToggle, isHorizontal
                 >
                     3D Portfolio
                 </motion.a>
-
-                <div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
+                {/* Botón hamburguesa */}
+                <button
+                    className="md:hidden text-white text-3xl mr-2"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    ☰
+                </button>
+                {/* Menu Desktop */}
+                <div className="max-w-6xl mx-auto px-4 py-4  justify-between items-center hidden md:flex ">
                     <div className="space-x-6 mx-auto">
                         {["about", "experience", "education", "projects", "skills", "certifications", "contact"].map((section) => (
                             <motion.a
@@ -58,12 +69,39 @@ export default function NavigationBar({ darkMode, onDarkModeToggle, isHorizontal
                         ))}
                     </div>
                 </div>
+                {/* Menu Mobile */}
+                <AnimatePresence>
+                    {menuOpen && (
+                        <motion.div
+                            initial={{ height: 0, opacity: 0 }}
+                            animate={{ height: "auto", opacity: 1 }}
+                            exit={{ height: 0, opacity: 0 }}
+                            transition={{ duration: 0.4 }}
+                            className={`${darkMode ? "bg-gray-900" : "bg-amber-950/80"} md:hidden fixed top-[40px] left-0 w-full  px-6 py-4 space-y-4`}
+                        >
+                            {sections.map((section) => (
+                                <a
+                                    key={section}
+                                    href="#"
+                                    onClick={(event) => handleNavBarClick(event, section)}
+                                    className={`block text-lg rounded-xl py-2 px-4 transition-colors ${darkMode
+                                        ? "hover:bg-gray-800 text-white/90"
+                                        : "hover:bg-amber-800 text-white"
+                                        }`}
+                                >
+                                    {section.charAt(0).toUpperCase() + section.slice(1)}
+                                </a>
+                            ))}
+                        </motion.div>
+                    )}
+
+                </AnimatePresence>
             </nav>
 
             {/* Botón modo oscuro */}
             <button
                 onClick={() => onDarkModeToggle(!darkMode)}
-                className={`fixed top-2 right-4 px-4 py-4 w-10 h-10 flex items-center justify-center 
+                className={`fixed md:top-2 md:right-4 md:w-10 md:h-10 top-1 right-14 px-4 py-4 w-6 h-6 flex items-center justify-center 
       text-white rounded-full shadow-md z-[100] transition duration-300 opacity-95 overflow-hidden
       ${darkMode ? "bg-gray-900 border-gray-500 hover:border-2" : "bg-[#96d1ff] border-[#3daaff] hover:border-2"}`}
             >
@@ -97,7 +135,7 @@ export default function NavigationBar({ darkMode, onDarkModeToggle, isHorizontal
             {/* Botón para cambiar entre horizontal/vertical */}
             <button
                 onClick={() => onHorizontalToggle(!isHorizontal)}
-                className={`${darkMode ? "bg-gray-700 hover:bg-gray-600" : "bg-amber-800 hover:bg-amber-700"} fixed top-2 right-20 px-4 py-2 bg-amber-800 text-white rounded-full shadow-md z-[100] hover:bg-amber-700 transition duration-300 opacity-95`}
+                className={`${darkMode ? "bg-gray-700 hover:bg-gray-600" : "bg-amber-800 hover:bg-amber-700"} hidden md:inline-flex fixed top-2 right-20 px-4 py-2 bg-amber-800 text-white rounded-full shadow-md z-[100] hover:bg-amber-700 transition duration-300 opacity-95`}
             >
                 {isHorizontal ? "Horizontal" : "Vertical"}
             </button>
@@ -110,7 +148,7 @@ export default function NavigationBar({ darkMode, onDarkModeToggle, isHorizontal
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: 0 }}
                 transition={{ duration: 1 }}
-                className={`${darkMode ? "bg-gray-700 hover:bg-gray-600" : "bg-amber-800 hover:bg-amber-700"} fixed top-2 right-60 z-[100] hover:bg-amber-700 rounded-2xl px-4 py-2 inline-flex items-center transition-all duration-300 hover:opacity-100`}
+                className={`${darkMode ? "bg-gray-700 hover:bg-gray-600" : "bg-amber-800 hover:bg-amber-700"} hidden md:inline-flex fixed top-2 right-60 z-[100] hover:bg-amber-700 rounded-2xl px-4 py-2 items-center transition-all duration-300 hover:opacity-100`}
             >
                 Download Resume <img src="/icons/download-white.svg" className="w-6 ml-2" />
             </motion.a>
